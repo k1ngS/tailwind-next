3fdb82cd3a09c847456d85e218c9936f4b852144
k1ngS <totonhorock@gmail.com>
2023-11-09 13:36:17 -0300

    "add: animate portfolio"

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,24 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        src="https://github.com/k1ngs.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Marcos Beltrão
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          marcos.beltrao@outlook.pt
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
