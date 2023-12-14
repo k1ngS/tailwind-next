d7f701d1fa9912e69c2910a8bb143e55169f64cc
Marcos Antonio <totonhorock@gmail.com>
2023-12-14 15:27:35 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,8 +35,8 @@ const config: Config = {
 
       keyframes: {
         slideDownAndFade: {
-          from: { opacity: 0, transform: 'translateY(-2px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
+          'from': { opacity: 0, transform: 'translateY(-2px)' },
+          'to': { opacity: 1, transform: 'translateY(0)' },
         },
         // slideUpAndFade: {
         //   from: { opacity: 1 },
